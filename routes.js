fc0197eefc37ab5d23f28ccb8fabf99560626046
sanoobtv <sanoob.tv@gmail.com>
2017-11-04 19:08:54 +1100

    Logic implentation on a html form

--- routes.js
+++ routes.js
@@ -1,25 +1,54 @@
 module.exports = function(app) {
+
   var bodyParser = require('body-parser');
   var isJSON = require('is-json');
   var jsonQuery = require('json-query')
+
   app.use(bodyParser.urlencoded({
     extended: true
   }));
   app.use(bodyParser.json());
+  //HomePage Rendering, a simple Form
   app.get('/', function(req, res) {
     res.render('Index.ejs');
   });
 
+//Form action from index.js.
   app.post('/validateJson', function(req, res) {
+    //obtain form data - which is JSON inside the text field.
     var data = req.body.inputJSON;
-    if (isJSON(data))
-    {
+  //validate if the given string is Json format- if true do the logic
+    if (isJSON(data)) {
       console.log('valid json');
+      var respon = getDataByDrm(data);
+      var episodefilter = getDataByEp(respon)
 
-console.log(jsonQuery('payload[country=USA].country',{data: data}));
-    res.render('Index.ejs')
-    } else {
+      function getDataByDrm(data) {
+
+        var jdata = JSON.parse(data);
+        var drm = true;
+        return jdata.payload.filter(
+          function(jdata) {
+            return jdata.drm == drm;
+          });
+      }
+
+      function getDataByEp(jrespon) {
+        return jrespon.filter(
+          function(jrespon) {
+            return jrespon.episodeCount > 0;
+          }
+        );
+      }
+      for (var i = 0; i < episodefilter.length; i++)
+        console.log(episodefilter[i].title);
+      res.render('Index.ejs')
+    }
+//if not json send error message, mentioning its not JSON
+    else {
       console.log('not valid');
     }
   });
+
+
 }
