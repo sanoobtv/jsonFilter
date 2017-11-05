fff82515822a4ca3d31b6731b3a9d07821147984
sanoobtv <sanoob.tv@gmail.com>
2017-11-05 19:46:04 +1100

    Changing json validator NPM

--- routes.js
+++ routes.js
@@ -1,19 +1,21 @@
 module.exports = function(app) {
 
   var bodyParser = require('body-parser');
-  var isJSON = require('is-json');
+  //var isJSON = require('is-json');
+  var isJSON = require('is-valid-json');
   var jsonQuery = require('json-query');
-  var oresultSet=require('./Model/resultSet.js');
+
+var oresultSet=require('./Model/resultSet.js');
 
   app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-//HomePage Rendering, a simple Form
-/*app.get('/', function(req, res) {
-  res.render('Index.ejs');
-});
-*/
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  //HomePage Rendering, a simple Form
+ app.get('/Home', function(req, res) {
+    res.render('Index.ejs');
+  });
+
   //Form action from index.js.
   app.post('/validateJson', function(req, res) {
     //obtain form data - which is JSON inside the text field.
@@ -51,61 +53,50 @@ app.use(bodyParser.json());
     }
   });
 
-
-
   app.post('/', (req, res ,err) => {
     var drm = req.body.drm;
     var episodeCount = req.body.episodeCount;
     var data = req.body.data;
 
-    if (err)
-    {
-    res.send({
-      'status': 'Error!! Could not decode request: JSON parsing failed'
-    });
-    }
-
     if (isJSON(data))
     {
-        console.log('valid json');
-        var respon = getDataByDrm(data);
-        var episodefilter = getDataByEp(respon)
-        var result = [];
+    console.log('valid json');
+    var respon = getDataByDrm(data);
+    var episodefilter = getDataByEp(respon)
+    var result = [];
 
-        function getDataByDrm(data) {
-          var jdata = JSON.parse(data);
-          var drm = true;
-          return jdata.payload.filter(
-            function(jdata) {
-              return jdata.drm == drm;
-              });
-            }
-
-        function getDataByEp(jrespon) {
-          return jrespon.filter(
-            function(jrespon) {
-              return jrespon.episodeCount > 0;
-            }
-          );
-        }
-
-        for (var i = 0; i < episodefilter.length; i++)
-        {
-         var iresultSet = new oresultSet(episodefilter[i].image.showImage, episodefilter[i].slug, episodefilter[i].title)
-         result[i] = iresultSet;
-        }
-
-        res.send({
-          'response': result,
-          'status': 'Sucsess'
+    function getDataByDrm(data) {
+      var jdata = JSON.parse(data);
+      var drm = true;
+      return jdata.payload.filter(
+        function(jdata) {
+          return jdata.drm == drm;
         });
     }
 
+    function getDataByEp(jrespon) {
+      return jrespon.filter(
+        function(jrespon) {
+          return jrespon.episodeCount > 0;
+        }
+      );
+    }
+    for (var i = 0; i < episodefilter.length; i++) {
+    var iresultSet = new oresultSet(episodefilter[i].image.showImage, episodefilter[i].slug, episodefilter[i].title)
+    result[i] = iresultSet;
+  }
+  res.send({
+    'response': result,
+    'status': 'Sucsess'
+
+  });
+  }
+
   else
   {
-    res.send({
-    'status':'Error!! Could not decode request: JSON parsing failed'
-     });
+  res.send({
+  'status':'Error!! Could not decode request: JSON parsing failed'
+  });
   }
 
 
