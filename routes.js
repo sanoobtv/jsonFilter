2ec0ca7ff79b69981451a87d6372e648a6b4c8e9
sanoobtv <sanoob.tv@gmail.com>
2017-11-09 14:38:20 +1100

    A

--- routes.js
+++ routes.js
@@ -57,7 +57,7 @@ module.exports = function(app) {
    {
      console.log(err);
      }
-  var data = req.body;
+  var data = JSON.stringify(req.body);
   console.log("input data is JSON");
     if (isJSON(data))
     {
