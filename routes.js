769134017725dedbaf3b3a964f7c6b4899257a85
sanoobtv <sanoob.tv@gmail.com>
2017-11-09 14:56:29 +1100

    Z

--- routes.js
+++ routes.js
@@ -69,6 +69,10 @@ module.exports = function(app) {
 
     function getDataByDrm(data) {
       console.log("1function");
+      console.log("!!!!!!!!!!!!!!!!!!!!");
+      console.log(JSON.parse(data));
+      console.log("!!!!!!!!!!!!!!!!!!!!");
+
       var jdata = JSON.parse(data);
       var drm = true;
       return jdata.filter(
